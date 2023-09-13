d99720e447843027b4f4670ad2aa4174d949a7af
Lee Shiueh <lee.shiueh@gmail.com>
2023-09-14 07:53:44 +0800

    Add setup.py module and refactoring cli.py

---
+++ src/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+
+def read_files(files):
+    data = []
+    for file in files:
+        with open(file, encoding='utf-8') as f:
+            data.append(f.read())
+    return "\n".join(data)
+
+
+long_description = read_files(['README.md', 'CHANGELOG.md'])
+
+setup(
+    name="pyswitchbot",
+    version="0.0.1",
+    description="A Python library for OpenWonderLabs SwitchBot API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    url="https://github.com/slee124565",
+    author="Lee Shiueh",
+    author_email="lee.shiueh@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "python-dotenv",
+        "requests",
+    ],
+    python_requires='>=3.11',
+    entry_points={
+        "console_scripts": [
+            "switchbot=switchbot.cli:switchybotcli",
+        ],
+    },
+)
