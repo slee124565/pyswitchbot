0d7cf654dafd330856cc3a670b5b64579e134289
Lee Shiueh <lee.shiueh@gmail.com>
2023-09-19 19:37:25 +0800

    Refactor setup.py config value source

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from switchbot import __title__
+from switchbot import __email__
+from switchbot import __version__
+from switchbot import __author__
 
 
 def read_files(files):
@@ -12,8 +16,8 @@ def read_files(files):
 # long_description = read_files(['README.md', 'CHANGELOG.md'])
 
 setup(
-    name="pyswitchbot-openapi",
-    version="0.0.1",
+    name=__title__,
+    version=__version__,
     description="A Python library for OpenWonderLabs SwitchBot API",
     long_description='long_description',
     long_description_content_type="text/markdown",
@@ -22,8 +26,8 @@ setup(
         "License :: OSI Approved :: MIT License",
     ],
     url="https://github.com/slee124565",
-    author="Lee Shiueh",
-    author_email="lee.shiueh@gmail.com",
+    author=__author__,
+    author_email=__email__,
     license="MIT",
     packages=find_packages(),
     install_requires=[
