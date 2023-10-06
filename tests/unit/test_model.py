d9c20ddd4a1e5fbffc2877dab02b3e862424b8d9
Lee Shiueh <lee.shiueh@gmail.com>
2023-10-06 10:01:09 +0800

    Backup

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -2,21 +2,23 @@ from switchbot.domain.model import SwitchBotUser
 from switchbot.domain.model import SwitchBotDevice
 
 
-def test_new_device_added_for_user():
-    """
-    原本用戶設備為0，經過查詢得到用戶設備為2，經過sync之後，用戶設備為2
-    """
+def test_sync_device_new_added_for_user():
+    """設備新增"""
     devices = [
-        SwitchBotDevice(device_id='6055F92FCFD2',
-                        device_name='小風扇開關',
-                        device_type='Plug Mini (US)',
-                        enable_cloud_service=True,
-                        hub_device_id=''),
-        SwitchBotDevice(device_id='6055F930FF22',
-                        device_name='風扇開關',
-                        device_type='Plug Mini (US)',
-                        enable_cloud_service=True,
-                        hub_device_id=''),
+        SwitchBotDevice(
+            device_id='6055F92FCFD2',
+            device_name='小風扇開關',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        ),
+        SwitchBotDevice(
+            device_id='6055F930FF22',
+            device_name='風扇開關',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        ),
     ]
     user = SwitchBotUser(secret='secret', token='token', devices=[])
     user.sync(devices=devices)
@@ -24,30 +26,42 @@ def test_new_device_added_for_user():
     assert set([dev.device_id for dev in user.devices]) == {'6055F92FCFD2', '6055F930FF22'}
 
 
-def test_device_updated_for_user():
+def test_sync_device_updated_for_user():
     """設備名稱變更"""
-    sync_devices = [SwitchBotDevice(device_id='6055F92FCFD2',
-                                    device_name='小風扇開關',
-                                    device_type='Plug Mini (US)',
-                                    enable_cloud_service=True,
-                                    hub_device_id=''),
-                    SwitchBotDevice(device_id='6055F930FF22',
-                                    device_name='床頭燈',
-                                    device_type='Plug Mini (US)',
-                                    enable_cloud_service=True,
-                                    hub_device_id='')]
+    sync_devices = [
+        SwitchBotDevice(
+            device_id='6055F92FCFD2',
+            device_name='小風扇開關',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        ),
+        SwitchBotDevice(
+            device_id='6055F930FF22',
+            device_name='床頭燈',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        )
+    ]
     user = SwitchBotUser(
         secret='secret', token='token',
-        devices=[SwitchBotDevice(device_id='6055F92FCFD2',
-                                 device_name='小風扇開關',
-                                 device_type='Plug Mini (US)',
-                                 enable_cloud_service=True,
-                                 hub_device_id=''),
-                 SwitchBotDevice(device_id='6055F930FF22',
-                                 device_name='風扇開關',
-                                 device_type='Plug Mini (US)',
-                                 enable_cloud_service=True,
-                                 hub_device_id='')]
+        devices=[
+            SwitchBotDevice(
+                device_id='6055F92FCFD2',
+                device_name='小風扇開關',
+                device_type='Plug Mini (US)',
+                enable_cloud_service=True,
+                hub_device_id=''
+            ),
+            SwitchBotDevice(
+                device_id='6055F930FF22',
+                device_name='風扇開關',
+                device_type='Plug Mini (US)',
+                enable_cloud_service=True,
+                hub_device_id=''
+            )
+        ]
     )
     user.sync(devices=sync_devices)
     for dev in user.devices:
@@ -60,24 +74,35 @@ def test_device_updated_for_user():
     assert len(user.devices) == 2
 
 
-def test_device_removed_for_user():
-    sync_devices = [SwitchBotDevice(device_id='6055F92FCFD2',
-                                    device_name='小風扇開關',
-                                    device_type='Plug Mini (US)',
-                                    enable_cloud_service=True,
-                                    hub_device_id='')]
+def test_sync_device_removed_for_user():
+    """設備移除"""
+    sync_devices = [
+        SwitchBotDevice(
+            device_id='6055F92FCFD2',
+            device_name='小風扇開關',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        )
+    ]
     user = SwitchBotUser(
         secret='secret', token='token',
-        devices=[SwitchBotDevice(device_id='6055F92FCFD2',
-                                 device_name='小風扇開關',
-                                 device_type='Plug Mini (US)',
-                                 enable_cloud_service=True,
-                                 hub_device_id=''),
-                 SwitchBotDevice(device_id='6055F930FF22',
-                                 device_name='風扇開關',
-                                 device_type='Plug Mini (US)',
-                                 enable_cloud_service=True,
-                                 hub_device_id='')]
+        devices=[
+            SwitchBotDevice(
+                device_id='6055F92FCFD2',
+                device_name='小風扇開關',
+                device_type='Plug Mini (US)',
+                enable_cloud_service=True,
+                hub_device_id=''
+            ),
+            SwitchBotDevice(
+                device_id='6055F930FF22',
+                device_name='風扇開關',
+                device_type='Plug Mini (US)',
+                enable_cloud_service=True,
+                hub_device_id=''
+            )
+        ]
     )
     user.sync(devices=sync_devices)
     assert len(user.devices) == 1
@@ -87,17 +112,24 @@ def test_device_removed_for_user():
     assert device.device_type == 'Plug Mini (US)'
 
 
-def test_device_no_changed_for_user():
-    sync_devices = [SwitchBotDevice(device_id='6055F92FCFD2',
-                                    device_name='小風扇開關',
-                                    device_type='Plug Mini (US)',
-                                    enable_cloud_service=True,
-                                    hub_device_id=''),
-                    SwitchBotDevice(device_id='6055F930FF22',
-                                    device_name='床頭燈',
-                                    device_type='Plug Mini (US)',
-                                    enable_cloud_service=True,
-                                    hub_device_id='')]
+def test_sync_device_no_changed_for_user():
+    """設備維持不變"""
+    sync_devices = [
+        SwitchBotDevice(
+            device_id='6055F92FCFD2',
+            device_name='小風扇開關',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        ),
+        SwitchBotDevice(
+            device_id='6055F930FF22',
+            device_name='床頭燈',
+            device_type='Plug Mini (US)',
+            enable_cloud_service=True,
+            hub_device_id=''
+        )
+    ]
     user = SwitchBotUser(secret='secret', token='token', devices=sync_devices)
     user.sync(devices=sync_devices)
     assert len(user.devices) == 2
