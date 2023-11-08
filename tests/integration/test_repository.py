a2cf59654fd95376b62a26d003530b9a021f6737
Lee Shiueh <lee.shiueh@gmail.com>
2023-11-08 13:56:50 +0800

    Add repository integration test: TestJsonFileRepository

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,3 +1,40 @@
-from switchbot.adapters import repository
+from switchbot.adapters import repository, orm_json
 from switchbot.domain import model
 
+
+class TestJsonFileRepository:
+    def test_get_by_dev_id(self):
+        session = orm_json.MarshmallowSchemaConverter(file='.test')
+        repo = repository.JsonFileRepository(session=session)
+        d = model.SwitchBotDevice(
+            device_id='did-1',
+            device_name='dev_name',
+            device_type='devType',
+            enable_cloud_service=True,
+            hub_device_id='hubDeviceId'
+        )
+        s = model.SwitchBotStatus(
+            device_id='did-1',
+            device_type='devType',
+            hub_device_id='hubDeviceId'
+        )
+        c = model.SwitchBotChangeReport(
+            event_type='eventType',
+            event_version='eventVersion',
+            context={
+                "deviceType": "WoPlugUS",
+                "deviceMac": "did-1",
+                "powerState": "ON",
+                "timeOfSample": 1698720698088
+            }
+        )
+        u = model.SwitchBotUserRepo(
+            uid='uid', secret='secret', token='token',
+            devices=[d], changes=[c], states=[s], scenes=[], webhooks=[]
+        )
+        repo.register(u)
+        assert repo.get_by_secret('secret') == u
+        assert repo.get_by_uid('uid') == u
+        assert repo.get_by_dev_id('did-1') == u
+        assert repo.get_dev_last_change_report(uid='uid', dev_id='did-1') == c
+        assert repo.get_dev_state(uid='uid', dev_id='did-1') == s
