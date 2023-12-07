2becf37d498f0fae8b1f544512492627b0ea6368
Lee Shiueh <lee.shiueh@gmail.com>
2023-12-07 16:39:21 +0800

    Fix integration unit test bugs caused by refactoring

--- tests/integration/test_views.py
+++ tests/integration/test_views.py
@@ -20,58 +20,28 @@ def memory_bus():
 def test_user_profile_by_uid_view(memory_bus):
     memory_bus.handle(commands.Register(secret='secret1', token='token1'))
     u = memory_bus.uow.users.get_by_secret('secret1')
-    memory_bus.handle(commands.RequestSync(uid=u.uid, devices=[
-        {
-            "deviceId": "6055F92FCFD2",
-            "deviceName": "小風扇開關",
-            "deviceType": "Plug Mini (US)",
-            "enableCloudService": True,
-            "hubDeviceId": ""
-        },
-        {
-            "deviceId": "6055F930FF22",
-            "deviceName": "風扇開關",
-            "deviceType": "Plug Mini (US)",
-            "enableCloudService": True,
-            "hubDeviceId": ""
-        }
-    ]))
 
     data = views.user_profile_by_uid(uid=u.uid, uow=memory_bus.uow)
+
     logger.debug(f'views.user_profile_by_uid {data}')
     assert isinstance(data, dict)
     assert data.get("uid") == u.uid
     assert data.get("devices") == 2
-    assert data.get("states") == 0
+    assert data.get("states") == 2
     assert data.get("changes") == 0
 
 
 def test_user_profile_by_secret_view(memory_bus):
     memory_bus.handle(commands.Register(secret='secret1', token='token1'))
     u = memory_bus.uow.users.get_by_secret('secret1')
-    memory_bus.handle(commands.RequestSync(uid=u.uid, devices=[
-        {
-            "deviceId": "6055F92FCFD2",
-            "deviceName": "小風扇開關",
-            "deviceType": "Plug Mini (US)",
-            "enableCloudService": True,
-            "hubDeviceId": ""
-        },
-        {
-            "deviceId": "6055F930FF22",
-            "deviceName": "風扇開關",
-            "deviceType": "Plug Mini (US)",
-            "enableCloudService": True,
-            "hubDeviceId": ""
-        }
-    ]))
 
     data = views.user_profile_by_secret(secret=u.secret, uow=memory_bus.uow)
+
     logger.debug(f'views.user_profile_by_uid {data}')
     assert isinstance(data, dict)
     assert data.get("uid") == u.uid
     assert data.get("devices") == 2
-    assert data.get("states") == 0
+    assert data.get("states") == 2
     assert data.get("changes") == 0
 
 
